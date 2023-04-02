bf04c5e7e6a2ab6879ce18667b3a0a386a9c4552
Tripatjot Singh <101708792+tripat1803@users.noreply.github.com>
2023-04-02 13:45:44 +0530

    Update Footer.jsx

--- src/Pages/Home/components/Footer.jsx
+++ src/Pages/Home/components/Footer.jsx
@@ -6,18 +6,18 @@ const Footer = () => {
 	return (
 		<footer
 			id="footer"
-			className="w-full py-10 md:py-0 md:h-[70vh] bg-[#fae0bf] flex justify-around items-center"
+			className="w-full px-8 py-10 gap-[16px] bg-[#fae0bf] flex justify-center items-center"
 		>
 			<img
-				className="w-[75%] sm:w-[55%] md:w-[25%]"
+				className="hidden lg:flex w-[75%] sm:w-[55%] md:w-[25%]"
 				src={FootImg}
 				alt="winter is comming"
 				width="25%"
 			/>
 
-			<div className="hidden md:flex text-[#824a23] justify-center gap-[4rem] [&_h3]:text-[120%] [&_h3]:font-[700] [&_p]:font-[500] [&_p]:hover:cursor-pointer transition-all duration-500">
-				<div>
-					<div>
+			<div className="w-[90%] lg:w-[60%] flex flex-col md:flex-row text-[#824a23] justify-center gap-[4rem] [&_h3]:text-[120%] [&_h3]:font-[700] [&_p]:font-[500] [&_p]:hover:cursor-pointer transition-all duration-500">
+				<div className="flex flex-col xxs:flex-row gap-[4rem]">
+					<div className="w-[50%]">
 						<h3>Shop</h3>
 						<p className="hover:scale-105">All</p>
 						<p className="hover:scale-105">Hot Chocolate</p>
@@ -25,25 +25,27 @@ const Footer = () => {
 						<p className="hover:scale-105">shipment</p>
 						<p className="hover:scale-105">Accessories</p>
 					</div>
+					<div className="w-[50%]">
+						<h3 className="text-[120%] font-[700]">Support</h3>
+						<p className="hover:scale-105">Return</p>
+						<p className="hover:scale-105">Shipping</p>
+						<p className="hover:scale-105">Privacy Policy</p>
+						<p className="hover:scale-105">Turms of Use</p>
+					</div>
 				</div>
-				<div>
-					<h3 className="text-[120%] font-[700]">Support</h3>
-					<p className="hover:scale-105">Return</p>
-					<p className="hover:scale-105">Shipping</p>
-					<p className="hover:scale-105">Privacy Policy</p>
-					<p className="hover:scale-105">Turms of Use</p>
-				</div>
-				<div>
-					<h3 className="text-[120%] font-[700]">Follow</h3>
-					<p className="hover:scale-105">Instagram</p>
-					<p className="hover:scale-105">Facebook</p>
-				</div>
-				<div>
-					<h3 className="text-[120%] font-[700]">Learn more</h3>
-					<p className="hover:scale-105">Research Hub</p>
-					<p className="hover:scale-105">FAQ's</p>
-					<p className="hover:scale-105">About Us</p>
-					<p className="hover:scale-105">Contect Us</p>
+				<div className="flex flex-col xxs:flex-row gap-[4rem]">
+					<div className="w-[50%]">
+						<h3 className="text-[120%] font-[700]">Follow</h3>
+						<p className="hover:scale-105">Instagram</p>
+						<p className="hover:scale-105">Facebook</p>
+					</div>
+					<div className="w-[50%]">
+						<h3 className="text-[120%] font-[700]">Learn more</h3>
+						<p className="hover:scale-105">Research Hub</p>
+						<p className="hover:scale-105">FAQ's</p>
+						<p className="hover:scale-105">About Us</p>
+						<p className="hover:scale-105">Contect Us</p>
+					</div>
 				</div>
 			</div>
 		</footer>
