114984bab7ad66aef925812e33b94da8f9726177
w-udhav <udhavwadhawan@hotmail.com>
2023-04-02 22:15:28 +0530

    update: product page image width

--- src/Pages/Product/components/Page.jsx
+++ src/Pages/Product/components/Page.jsx
@@ -6,7 +6,6 @@ import Lottie from "lottie-react";
 import Reviews from "./Reviews";
 
 const Page = ({ data, id, related, setFlag, loader2 }) => {
-
   // const imageArr = [require("../../../Assets/CofCar.jpg"), require("../../../Assets/CofCar.jpg"), require("../../../Assets/hotChoco.png")];
   const [imageArr, setImageArr] = useState([]);
   const [image, setImage] = useState();
@@ -46,16 +45,17 @@ const Page = ({ data, id, related, setFlag, loader2 }) => {
         <div className="flex flex-col lg:flex-row items-start justify-center mx-[10%] lg:space-x-20">
           {/* Product Image */}
           <div className="w-full lg:w-[50%] flex flex-col md:flex-row lg:flex-col space-y-10 items-center md:justify-around lg:justify-center pb-16 lg:pb-0 ">
-            {(image) ? <><div className="">
-              <img
-                className="w-[15rem] md:w-[25rem] lg:w-[35rem] rounded-full"
-                src={image}
-                alt="product"
-              />
-            </div>
-              <div className="flex md:flex-col lg:flex-row space-x-2 md:space-y-4 lg:space-y-0 lg:space-x-2 items-center justify-center overflow-auto">
-                {
-                  imageArr?.map((item, index) => {
+            {image ? (
+              <>
+                <div className="">
+                  <img
+                    className="w-[15rem] md:w-[23rem] lg:w-[18rem] xl:w-[22rem] rounded-full"
+                    src={image}
+                    alt="product"
+                  />
+                </div>
+                <div className="flex md:flex-col lg:flex-row space-x-2 md:space-y-4 lg:space-y-0 lg:space-x-2 items-center justify-center overflow-auto">
+                  {imageArr?.map((item, index) => {
                     return (
                       <img
                         key={index}
@@ -66,10 +66,13 @@ const Page = ({ data, id, related, setFlag, loader2 }) => {
                         src={item?.url}
                         alt="product"
                       />
-                    )
-                  })
-                }
-              </div></> : <div className="h-full">No Preview Available</div>}
+                    );
+                  })}
+                </div>
+              </>
+            ) : (
+              <div className="h-full">No Preview Available</div>
+            )}
           </div>
 
           {/* Details */}
@@ -78,35 +81,51 @@ const Page = ({ data, id, related, setFlag, loader2 }) => {
 
         <div className="flex flex-col items-center lg:mt-44 py-10">
           <h1 className="font-black text-2xl py-16">You may also like</h1>
-          <Box sx={loader && {
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-            {(!loader && relatedData) && <Box>
-              {
-                relatedData?.map((item) => {
-                  return (
-                      <ProductCard key={item._id} id={item._id} productDetails={item.details[0]} imageUrl={item.image[0]?.url} title={item.name} setFlag={setFlag} />
-                  );
-                })
-              }
-            </Box>}
-            {
-              loader && <Box sx={{
-                height: "200px",
+          <Box
+            sx={
+              loader && {
                 display: "flex",
                 justifyContent: "center",
-                alignItems: "center"
-              }}>
-                <Lottie animationData={require("../../../Assets/Lottie/loader.json")} style={{
-                  width: "150px"
-                }} />
-              </Box>
+                alignItems: "center",
+              }
             }
+          >
+            {!loader && relatedData && (
+              <Box>
+                {relatedData?.map((item) => {
+                  return (
+                    <ProductCard
+                      key={item._id}
+                      id={item._id}
+                      productDetails={item.details[0]}
+                      imageUrl={item.image[0]?.url}
+                      title={item.name}
+                      setFlag={setFlag}
+                    />
+                  );
+                })}
+              </Box>
+            )}
+            {loader && (
+              <Box
+                sx={{
+                  height: "200px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Lottie
+                  animationData={require("../../../Assets/Lottie/loader.json")}
+                  style={{
+                    width: "150px",
+                  }}
+                />
+              </Box>
+            )}
           </Box>
         </div>
-        
+
         <Reviews productId={productId} total={productData?.totalReviews} />
       </div>
     </>
