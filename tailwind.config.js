d23cff645e8dd09b77f0112eb03a2d808dc4a9da
Tripatjot Singh <101708792+tripat1803@users.noreply.github.com>
2023-04-02 14:19:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	screens: {
+		'xxs': "350px",
+		'sm': '640px',
+		'md': '768px',
+		'lg': '1024px',
+		'xl': '1280px',
+		'2xl': '1536px',
+	},
 	theme: {
 		extend: {
 			boxShadow: {
