b3d72d660469b6abe1db2375fcffc808084d7bcc
IshanSingla <is.ishan.singla@gmail.com>
2023-04-03 10:28:19 +0530

    Update: Code cleaning | minor bugs fixed

--- src/Components/Global.js
+++ src/Components/Global.js
@@ -2,18 +2,20 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import Footer from "../Pages/Home/components/Footer";
 import Header from "../Pages/Home/components/Header";
+import Review from "../Pages/Home/components/Review";
+import Sub from "../Pages/Home/components/Sub";
 import HeaderNew from "./HeaderNew";
 
 export default function Global({ children }) {
-  const route = useLocation().pathname
+  const route = useLocation().pathname;
   var background;
-  if (route === "/about"|| route === "/cart") {
+  if (route === "/about" || route === "/cart") {
     background = "#FFF8EF";
   } else if (route === "/product") {
     background = "linear-gradient(180deg, #E4B579 0%, #FFF8EF 23.75%)";
   } else if (route === "/profile/account" || route === "/profile/order") {
     background = "rgba(255, 248, 239, 1)";
-  } 
+  }
   return (
     <main
       style={{
@@ -22,6 +24,8 @@ export default function Global({ children }) {
     >
       {route === "/" ? <Header /> : <HeaderNew />}
       {children}
+      {(route === "/" || route === "/shop") && <Review />}
+      {route === "/" && <Sub />}
       <Footer />
     </main>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,11 @@
 import Hero from "./components/Hero";
 import HomeProduct from "./components/HomeProduct";
-import Review from "./components/Review";
-import Sub from "./components/Sub";
 
 function Home() {
   return (
     <>
       <Hero />
       <HomeProduct />
-      <Review />
-      <Sub />
     </>
   );
 }
